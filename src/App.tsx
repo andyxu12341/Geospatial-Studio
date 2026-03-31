d9ef3e9b04f1e14b0339c334e8c8d517c26aed95
andy xu1 <andyxu1@andydeMacBook-Pro.local>
2026-04-01 03:12:45 +0800

    fix: 移除多余的 ThemeProvider 避免与 Index 内部 dark mode 冲突

--- src/App.tsx
+++ src/App.tsx
@@ -1,15 +1,12 @@
 import { BrowserRouter, Routes, Route } from "react-router-dom";
-import { ThemeProvider } from "next-themes";
 import Index from "./pages/Index";
 
 export default function App() {
   return (
     <BrowserRouter>
-      <ThemeProvider attribute="class" defaultTheme="system" enableSystem>
-        <Routes>
-          <Route path="/" element={<Index />} />
-        </Routes>
-      </ThemeProvider>
+      <Routes>
+        <Route path="/" element={<Index />} />
+      </Routes>
     </BrowserRouter>
   );
 }
